ddf444e387f8eae6b6bec9d8416122819b223cf0
Daniel Garrigan <daniel.garrigan@ancestry.com>
2018-11-13 20:44:02 -0800

    Fixed typo in prefix array indexing

--- pbwt.c
+++ pbwt.c
@@ -610,7 +610,7 @@ build_prefix_array (pbwt_t *b)
 			size_t ix;
 			size_t ms;
 
-			ix = b->ppa[i];
+			ix = b->ppa[j];
 			ms = b->div[j];
 
 			if (ms > da)
@@ -662,6 +662,83 @@ build_prefix_array (pbwt_t *b)
 int
 find_matches (pbwt_t *b, size_t query_index, size_t minlen)
 {
+	size_t i;
+	size_t j;
+	size_t k;
+	size_t ia;
+	size_t ib;
+	size_t da;
+	size_t db;
+	size_t *ara;
+	size_t *arb;
+	size_t *ard;
+	size_t *are;
+
+	ara = (size_t *) malloc (b->nsam * sizeof(size_t));
+	arb = (size_t *) malloc (b->nsam * sizeof(size_t));
+	ard = (size_t *) malloc (b->nsam * sizeof(size_t));
+	are = (size_t *) malloc (b->nsam * sizeof(size_t));
+
+	for (i = 0; i < b->nsite; ++i)
+	{
+		ia = 0;
+		ib = 0;
+		da = i + 1;
+		db = i + 1;
+
+		for (j = 0; j < b->nsam; ++j)
+		{
+			size_t ix;
+			size_t ms;
+
+			ix = b->ppa[j];
+			ms = b->div[j];
+
+			/*if (ms > i - minlen) */
+
+			if (ms > da)
+				da = ms;
+			if (ms > db)
+				db = ms;
+
+			if (b->data[TWODCORD(ix, b->nsite, i)] == '0')
+			{
+				ara[ia] = ix;
+				ard[ia] = da;
+				da = 0;
+				++ia;
+			}
+			else
+			{
+				arb[ib] = ix;
+				are[ib] = db;
+				db = 0;
+				++ib;
+			}
+		}
+
+		/* Concatenate arrays */
+		if (i < b->nsite - 1)
+		{
+			for (j = 0; j < ia; ++j)
+			{
+				b->ppa[j] = ara[j];
+				b->div[j] = ard[j];
+			}
+			for (j = 0, k = ia; j < ib; ++j, ++k)
+			{
+				b->ppa[k] = arb[j];
+				b->div[k] = are[j];
+			}
+		}
+	}
+
+	/* Free allocated memory */
+	free (ara);
+	free (arb);
+	free (ard);
+	free (are);
+
 	return 0;
 }
 
