f5e10702d937b49923c8f118a2d34d87d425ea35
Daniel Garrigan <dang@evolgen.us>
2018-11-16 10:30:34 -0800

    pbwt_print_match now outputs genetic map positions of matches

--- pbwt_print_match.c
+++ pbwt_print_match.c
@@ -9,8 +9,8 @@ pbwt_print_match (pbwt_t *b, match_t *list)
     {
         match_t *p;
         for (p = list; p != NULL; p = p->next)
-            printf ("%s\t%s\t%zu\t%zu\n",
-                    b->sid[p->first], b->sid[p->second], p->begin, p->end);
+            printf ("%s\t%s\t%zu\t%zu\t%1.5lf\t%1.5lf\n",
+                    b->sid[p->first], b->sid[p->second], p->begin, p->end, b->cm[p->begin], b->cm[p->end]);
     }
 
     return 0;
