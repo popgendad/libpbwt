84b7195826068fd7b0c616d7a66fd86db2a370d5
Daniel Garrigan <dang@evolgen.us>
2018-12-04 21:33:52 -0800

    Test program now evaluates query and pop-level matches

--- test/test.c
+++ test/test.c
@@ -7,8 +7,10 @@ int
 main (int argc, char *argv[])
 {
     int v;
+    size_t qid;
     pbwt_t *b;
     pbwt_t *s;
+    pbwt_t *p;
     const double minlen = 0.4;
     char *infile;
 
@@ -19,7 +21,15 @@ main (int argc, char *argv[])
         infile = strdup (argv[1]);
     else
     {
-        fputs ("Usage: ./ptest <pbwt file> <optional pop>\n", stderr);
+        fputs ("Usage: ./ptest <pbwt file> <query id>\n", stderr);
+        return 1;
+    }
+
+    if (argv[2] != NULL)
+        qid = atol (argv[2]);
+    else
+    {
+        fputs ("Usage: ./ptest <pbwt file> <query id>\n", stderr);
         return 1;
     }
 
@@ -37,9 +47,11 @@ main (int argc, char *argv[])
     size_t nregs;
     char **reglist;
     reglist = pbwt_get_reglist (b, &nregs);
+    printf ("Query: %s    %s\n", b->sid[qid], b->reg[qid]);
     for (i = 0; i < nregs; ++i)
     {
-        s = pbwt_subset (b, reglist[i]);
+        p = pbwt_subset (b, reglist[i]);
+        s = pbwt_subset_with_query (b, reglist[i], qid);
         if (s == NULL)
         {
             fputs ("Failed to subset the data", stderr);
@@ -48,17 +60,23 @@ main (int argc, char *argv[])
 
         /* Build the prefix and divergence arrays for the subset pbwt */
         v = pbwt_build (s);
+        v = pbwt_build (p);
 
         /* Find all set-maximal matches */
-        v = pbwt_set_match (s, minlen);
+        v = pbwt_query_match (s, minlen);
+        v = pbwt_set_match (p, minlen);
 
         double c;
-        c = match_coverage (s, s->match);
-        printf ("%s\t%1.5e\n", reglist[i], c);
+        double e;
+        c = match_query_coverage (s, s->match);
+        e = match_coverage (p, p->match);
+        printf ("%s\t%1.5e\t%1.5e\t%1.5lf\n", reglist[i], c, e, c / e);
 
         /* Free memory for the subset pbwt data structure */
         if (s != NULL)
             pbwt_destroy (s);
+        if (p != NULL)
+            pbwt_destroy (p);
     }
 
     /* Free memory for the original pbwt data structure */
