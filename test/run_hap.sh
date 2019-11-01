355deebdea7f3bb4c9ffeafd9d56fd461961e2c1
Daniel Garrigan <popgendad@gmail.com>
2019-11-01 14:56:44 -0700

    Updated some paths in the run_haps.sh script

--- test/run_hap.sh
+++ test/run_hap.sh
@@ -3,5 +3,5 @@ sampid=$1
 for i in $(seq 0 1374)
 do
 	k=$(printf "%04d" $i)
-	/tmp/test/ptest2 /data/EV92_${k}.pbwt 0.05 ${sampid} 0 399 > /data/sample_${sampid}_${k}.txt
+	/tmp/libpbwt/test/ptest2 /data/EV92_${k}.pbwt 0.05 ${sampid} 0 399 > /data/sample_${sampid}_${k}.txt
 done
