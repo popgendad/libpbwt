8bcfa01be645f874b684c4b6996f671608346b3e
Daniel Garrigan <popgendad@gmail.com>
2019-11-25 09:42:46 -0800

    Updated ptest and run_hap test routines

--- test/run_hap.sh
+++ test/run_hap.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 sampid=$1
-for i in $(seq 0 1374)
-do
-	k=$(printf "%04d" $i)
-	/tmp/libpbwt/test/ptest2 /data/EV92_${k}.pbwt 0.05 ${sampid} 0 399 > /data/sample_${sampid}_${k}.txt
-done
+/tmp/libpbwt/test/ptest2 /data/HumanOriginsExample.pbwt 0.5 ${sampid} 0 729 > /data/sample_${sampid}.txt
--- test/test2.c
+++ test/test2.c
@@ -5,18 +5,15 @@
 
 
 int
-main (int argc, char *argv[])
+main(int argc, char *argv[])
 {
-    int a;
-    int v;
-    size_t qid;
-    size_t qstart;
-    size_t qend;
-    pbwt_t *b;
-    pbwt_t *s;
-    pbwt_t *p;
-    double minlen = 0.1;
-    char *infile;
+    int v = 0;
+    size_t qid = 0;
+    size_t qstart = 0;
+    size_t qend = 0;
+    pbwt_t *b = NULL;
+    double minlen = 0.5;
+    char *infile = NULL;
     khint_t k;
     khash_t(floats) *result;
 
@@ -25,54 +22,55 @@ main (int argc, char *argv[])
 
     /* Define infile name */
     if (argv[1] != NULL)
-        infile = strdup (argv[1]);
+        infile = strdup(argv[1]);
     else
     {
-        fputs ("Usage: ./ptest <pbwt file>\n", stderr);
+        fputs("Usage: ./ptest2 <pbwt file> <minlen> <hap_index> <start> <end>\n", stderr);
         return 1;
     }
+
     minlen = atof(argv[2]);
     qid = (size_t)(atoi(argv[3]));
     qstart = (size_t)(atoi(argv[4]));
     qend = (size_t)(atoi(argv[5]));
+
     /* Read in the pbwt file from disk */
-    b = pbwt_read (infile);
+    b = pbwt_read(infile);
     if (b == NULL)
     {
-        fprintf (stderr, "Cannot read data from %s\n", infile);
+        fprintf(stderr, "Cannot read data from %s\n", infile);
         return 1;
     }
 
-    pbwt_uncompress (b);
+    pbwt_uncompress(b);
 
     /* Build the prefix and divergence arrays for the subset pbwt */
-    v = pbwt_build (b);
+    v = pbwt_build(b);
 
     b->is_query[qid] = 1;
 
     /* Find all set-maximal matches */
     v = pbwt_query_match(b, minlen);
 
-    /*pbwt_print(p);
-    match_print(p, p->match);*/
     match_search(b, b->match, result, qstart, qend);
 
     /* Print hash */
-    size_t i;
-    size_t nregs;
-    char **reglist;
-    reglist = pbwt_get_reglist (b, &nregs);
+    size_t i = 0;
+    size_t nregs = 0;
+    char **reglist = NULL;
+    reglist = pbwt_get_reglist(b, &nregs);
+
     for (i = 0; i < nregs; ++i)
     {
         k = kh_get(floats, result, reglist[i]);
         if (kh_exist(result, k) && k != kh_end(result))
-            fprintf (stdout, "%s\t%s\t%s\t%1.5lf\n", infile, b->reg[qid], reglist[i], kh_value(result, k));
+            fprintf(stdout, "%s\t%s\t%s\t%1.5lf\n", infile, b->reg[qid], reglist[i], kh_value(result, k));
         else
-            fprintf (stdout, "%s\t%s\t%s\t0.00000\n", infile, b->reg[qid], reglist[i]);
+            fprintf(stdout, "%s\t%s\t%s\t0.00000\n", infile, b->reg[qid], reglist[i]);
     }
 
     /* Free memory for the original pbwt data structure */
-    pbwt_destroy (b);
+    pbwt_destroy(b);
     free(reglist);
 
     return v;
