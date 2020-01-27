b5f857aea5973dd87ae2d3de3c13333201e3ce1f
Daniel Garrigan <popgendad@gmail.com>
2020-01-27 13:32:21 -0800

    Still trying to fix version reporting problem

--- src/pbwt_version.c
+++ src/pbwt_version.c
@@ -4,10 +4,10 @@
 #define stringy(x) xstringy(x)
 
 #ifdef VERSION
-const char libpbwt_version[] = stringy(VERSION);
+const char lpbwt_version[] = stringy(VERSION);
 #endif
 
 const char *libpbwt_version(void)
 {
-	return libpbwt_version;
+	return lpbwt_version;
 }
